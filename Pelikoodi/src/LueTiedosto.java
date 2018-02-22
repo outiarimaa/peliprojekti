1d6fd9da5fce976691e940f8ca4611e75bbf79ff
unknown <saarinensamueli@gmail.com>
2018-02-22 10:18:09 +0200

    Pelikoodin esimerkki

---
+++ Pelikoodi/src/LueTiedosto.java
@@ -0,0 +1,38 @@
+import sun.security.krb5.internal.PAData;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LueTiedosto {
+    public static void main(String[] args) {
+        String nimi = "LueTiedosto.java";
+        String polku = "src/";
+        try {
+            LueTiedosto(nimi,polku);
+        } catch (OmaPoikkeus e) {
+            System.err.println("Mainissa ongelma: " + e.getMessage());
+        }
+    }
+
+    public static void luetiedosto(String nimi , String polku) throws IOException {
+        Path tiedosto = Paths.get(polku, nimi);
+        List<String> rivit;
+        try {
+            rivit = Files.readAllLines(tiedosto);
+        } catch (IOException e) {
+            System.err.println("Saatiin poikkeus kiinni, heitetään eteenpäin.") {
+                    throw new OmaPoikkeus(e);
+                }
+            }
+    }
+}
+
+class OmaPoikkeus extends Exception {
+    public OmaPoikkeus() {
+        System.err.println("Virhe");
+
+    }
+}

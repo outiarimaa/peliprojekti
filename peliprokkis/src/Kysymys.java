2324527f14ac64c20c85df730bd270ae1f09280e
outiarimaa <outi.arimaa@gmail.com>
2018-02-22 13:14:48 +0200

    Luotu kysymys-luokka

--- peliprokkis/src/Kysymys.java
+++ peliprokkis/src/Kysymys.java
@@ -1,2 +1,37 @@
 public class Kysymys {
+    private int id;
+    private String teksti;
+
+    public Kysymys(){
+
+    }
+
+    public Kysymys(int id, String teksti) {
+        this.id = id;
+        this.teksti = teksti;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTeksti() {
+        return teksti;
+    }
+
+    public void setTeksti(String teksti) {
+        this.teksti = teksti;
+    }
+
+    @Override
+    public String toString() {
+        return "Kysymys{" +
+                "id=" + id +
+                ", teksti='" + teksti + '\'' +
+                '}';
+    }
 }

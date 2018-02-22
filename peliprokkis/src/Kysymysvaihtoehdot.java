77272eccdfc9b0d5feb901f80edd9a8906edfe21
Jokatlin <lindh.j.k@gmail.com>
2018-02-22 13:12:48 +0200

    Lisätty kysymysvaihtoehdot luokka

--- peliprokkis/src/Kysymysvaihtoehdot.java
+++ peliprokkis/src/Kysymysvaihtoehdot.java
@@ -1,2 +1,58 @@
 public class Kysymysvaihtoehdot {
+    private int id;
+    private int kysymysid;
+    private String teksti;
+    private String oikeavastaus;
+
+    public Kysymysvaihtoehdot() {
+    }
+
+    public Kysymysvaihtoehdot(int id, int kysymysid, String teksti, String oikeavastaus) {
+        this.id = id;
+        this.kysymysid = kysymysid;
+        this.teksti = teksti;
+        this.oikeavastaus = oikeavastaus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getKysymysid() {
+        return kysymysid;
+    }
+
+    public void setKysymysid(int kysymysid) {
+        this.kysymysid = kysymysid;
+    }
+
+    public String getTeksti() {
+        return teksti;
+    }
+
+    public void setTeksti(String teksti) {
+        this.teksti = teksti;
+    }
+
+    public String getOikeavastaus() {
+        return oikeavastaus;
+    }
+
+    public void setOikeavastaus(String oikeavastaus) {
+        this.oikeavastaus = oikeavastaus;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Kysymysvaihtoehdot{" +
+                "id=" + id +
+                ", kysymysid=" + kysymysid +
+                ", teksti='" + teksti + '\'' +
+                ", oikeavastaus='" + oikeavastaus + '\'' +
+                '}';
+    }
 }
